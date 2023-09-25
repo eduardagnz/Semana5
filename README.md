2c947265665855b199e00385bc0e0eb89210abbd
eduardagnz <eduarda.oliveira@sou.inteli.edu.br>
2023-09-25 04:31:25 -0300

    Atualização

---
+++ README.md
@@ -0,0 +1,34 @@
+# Elaboração de aplicação integrada a um banco de dados
+
+## Objetivo
+Instalar um servidor web Apache com PHP e criar um banco de dados MySQL. Tanto a instância do Amazon EC2 quanto a instância de banco de dados são executadas em uma nuvem privada virtual (VPC) baseada no serviço Amazon VPC.
+
+## 1- Tutorial
+
+Cria-se uma instancia em EC2 chamada "semana5":
+![Página inicial](./imagens/image.png)
+Aqui podemos ver o tipo da instancia segundo o tutorial (t2.micro):
+![Instancias](./imagens/image-2.png)
+Os dados da instancia "semana5":
+![Info da instancia criada](./imagens/image1.png)
+
+Com a instancia criada, cria-se uma instancia de banco de dados RDS, seguindo o tutorial.
+![RDS](./imagens/image3.png)
+![info RDS](./imagens/image4.png)
+![info importantes](./imagens/image5.png)
+
+A partir disso, instala-se um servidor web na instancia EC2, seguindo o tutorial.
+Conectamos o EC2
+![ssh](./imagens/image6.png)
+![putty](./imagens/image7.png)
+![Alt text](./imagens/finaltutorial.png)
+
+Apos isso, fazemos a alteração do código php disponibilizado e adcionamos mais 2 campos desejaveis.
+
+Link do drive que contem o video:
+*https://drive.google.com/drive/folders/1Wr_QoSDRtzPWidcRKx5YJF2zg8VfSqAK?usp=drive_link*
+
+
+
+
+
---
+++ scr/SamplePage.php
@@ -0,0 +1,130 @@
+<?php include "../inc/dbinfo.inc"; ?>
+<html>
+<body>
+<h1>Sample page</h1>
+<?php
+
+$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
+if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+$database = mysqli_select_db($connection, DB_DATABASE);
+
+VerifyDataTable($connection, DB_DATABASE);
+
+$data_nome = htmlentities($_POST['DATA_NOME']);
+$data_idade = htmlentities($_POST['DATA_IDADE']);
+$data_peso = htmlentities($_POST['DATA_PESO']);
+$data_altura = htmlentities($_POST['DATA_ALTURA']);
+
+if (strlen($data_nome) || strlen($data_idade) || strlen($data_peso) || strlen($data_altura)) {
+    AddData($connection, $data_nome, $data_idade, $data_peso, $data_altura);
+}
+?>
+
+<form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
+    <h2>Add Data</h2>
+    <table border="0">
+        <tr>
+            <td>Nome</td>
+            <td>Idade</td>
+            <td>Peso</td>
+            <td>Altura</td>
+        </tr>
+        <tr>
+            <td>
+                <input type="text" name="DATA_NOME" maxlength="45" size="30" />
+            </td>
+            <td>
+                <input type="text" name="DATA_IDADE" maxlength="100" size="60" />
+            </td>
+            <td>
+                <input type="text" name="DATA_PESO" maxlength="20" size="20" />
+            </td>
+            <td>
+                <input type="text" name="DATA_ALTURA" maxlength="90" size="60" />
+            </td>
+            <td>
+                <input type="submit" value="Add Data" />
+            </td>
+        </tr>
+    </table>
+</form>
+
+
+<table border="1" cellpadding="2" cellspacing="2">
+    <tr>
+        <td>ID</td>
+        <td>NOME</td>
+        <td>IDADE</td>
+        <td>PESO</td>
+        <td>ALTURA</td>
+    </tr>
+
+    <?php
+    $result = mysqli_query($connection, "SELECT * FROM DATA");
+
+    while ($query_data = mysqli_fetch_row($result)) {
+        echo "<tr>";
+        echo "<td>", $query_data[0], "</td>",
+        "<td>", $query_data[1], "</td>",
+        "<td>", $query_data[2], "</td>",
+        "<td>", $query_data[3], "</td>",
+        "<td>", $query_data[4], "</td>";
+        echo "</tr>";
+    }
+    ?>
+
+</table>
+
+<?php
+
+mysqli_free_result($result);
+mysqli_close($connection);
+
+?>
+
+</body>
+</html>
+
+<?php
+
+/* adiciona dados na tabela */
+function AddData($connection, $name, $email, $phone, $address) {
+   $n = mysqli_real_escape_string($connection, $nome);
+   $i = mysqli_real_escape_string($connection, $idade);
+   $p = mysqli_real_escape_string($connection, $peso);
+   $a = mysqli_real_escape_string($connection, $altura);
+
+    $query = "INSERT INTO DATA (NOME, IDADE, PESO, ALTURA) VALUES ('$n', '$e', '$p', '$a');";
+
+    if (!mysqli_query($connection, $query)) echo("<p>Error adding data.</p>");
+}
+
+function VerifyDataTable($connection, $dbName) {
+    if (!TableExists("DATA", $connection, $dbName)) {
+        $query = "CREATE TABLE DATA (
+            ID int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            NOME VARCHAR(45),
+            IDADE VARCHAR(2),
+            PESO int(4),
+            ALTURA int(3)
+          )"; 
+
+        if (!mysqli_query($connection, $query)) echo("<p>Error creating DATA table.</p>");
+    }
+}
+
+/* Check whether a table exists. */
+function TableExists($tableName, $connection, $dbName) {
+    $t = mysqli_real_escape_string($connection, $tableName);
+    $d = mysqli_real_escape_string($connection, $dbName);
+
+    $checktable = mysqli_query($connection,
+        "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'");
+
+    if (mysqli_num_rows($checktable) > 0) return true;
+
+    return false;
+}
+?>
